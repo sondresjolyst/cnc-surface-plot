67b297487da250f892da251081a4ca76ae3bf6fd
sondresjolyst <sosj@equinor.com>
2025-10-31 07:42:41 +0100

    chore: split points up in four sections. autosize figure

--- main.py
+++ main.py
@@ -4,17 +4,24 @@ import numpy as np
 from scipy.interpolate import griddata
 
 points = [
-    {"x": 1, "y": 10, "z": 4.8},  # Top-left corner
-    {"x": 10, "y": 1, "z": 4.2},  # Bottom-right corner
-    {"x": 10, "y": 10, "z": 5}, # Top-right corner
-    {"x": 1, "y": 1, "z": 4}, # Bottom-left corner
-    {"x": 5, "y": 5, "z": 4.5}, # Center point
+    # Center Section
+    {"x": 5, "y": 5, "z": 0},
+    # Top-Left to Center
+    {"x": 1, "y": 10, "z": 0},
+    # Top-Right to Center
+    {"x": 10, "y": 10, "z": 0},
+    # Bottom-Left to Center
+    {"x": 1, "y": 1, "z": 0},
+    # Bottom-Right to Center
+    {"x": 10, "y": 1, "z": 0},
 ]
 
 df = pd.DataFrame(points)
 
+grid_resolution = 50
 grid_x, grid_y = np.meshgrid(
-    np.linspace(df.x.min(), df.x.max(), 50), np.linspace(df.y.min(), df.y.max(), 50)
+    np.linspace(df.x.min(), df.x.max(), grid_resolution),
+    np.linspace(df.y.min(), df.y.max(), grid_resolution),
 )
 
 # Interpolate Z values to create surface (using linear for stability with few points)
@@ -24,11 +31,5 @@ sh_0, sh_1 = z.shape
 x, y = np.linspace(0, 1, sh_0), np.linspace(0, 1, sh_1)
 
 fig = go.Figure(data=[go.Surface(z=z, x=x, y=y)])
-fig.update_layout(
-    title=dict(text="CNC Bed Surface"),
-    autosize=False,
-    width=500,
-    height=500,
-    margin=dict(l=65, r=50, b=65, t=90),
-)
+fig.update_layout(title=dict(text="CNC Bed Surface"), autosize=True)
 fig.show()
